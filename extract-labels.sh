bbc3be9d8e1292ab3c2ce46f52d456c3c8795740
Tobias Kuhn <kuhntobias@gmail.com>
2020-05-12 20:34:02 +0200

    Fix script

--- extract-labels.sh
+++ extract-labels.sh
@@ -3,7 +3,7 @@
 TPRED='<http://data.europa.eu/eli/ontology#title_alternative>'
 LPRED='<http://www.w3.org/2000/01/rdf-schema#label>'
 
-rapper -i turtle -o ntriples gdpr.ttl \
+rapper -i turtle -o ntriples data/gdpr.ttl \
   | grep " $TPRED " \
   | sed "s| $TPRED | $LPRED |" \
   | less
