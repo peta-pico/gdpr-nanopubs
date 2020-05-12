d526324865eab6cf0d2cb2b6d88e0ca09f754aab
Tobias Kuhn <kuhntobias@gmail.com>
2020-05-12 20:32:39 +0200

    Add script and files

---
+++ extract-labels.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TPRED='<http://data.europa.eu/eli/ontology#title_alternative>'
+LPRED='<http://www.w3.org/2000/01/rdf-schema#label>'
+
+rapper -i turtle -o ntriples gdpr.ttl \
+  | grep " $TPRED " \
+  | sed "s| $TPRED | $LPRED |" \
+  | less
+#  > gdpr-labels.nt
