28d5368a5946ff4f3bc3ee317ca0ef06992de58c
Tobias Kuhn <kuhntobias@gmail.com>
2020-05-12 21:16:01 +0200

    Update label list and template

--- extract-labels.sh
+++ extract-labels.sh
@@ -6,4 +6,6 @@ LPRED='<http://www.w3.org/2000/01/rdf-schema#label>'
 rapper -i turtle -o ntriples data/gdpr.ttl \
   | grep " $TPRED " \
   | sed "s| $TPRED | $LPRED |" \
+  | sed -r 's|> "Article([0-9])|> "Article \1|' \
+  | sed -r 's|chapter([IVX]+)-(.*)> "Section |chapter\1-\2> "Chapter \1, Section |' \
   > gdpr-labels.nt
