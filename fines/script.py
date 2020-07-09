9fee58e5b9e665504fed176e15985b55b1080bcb
Tobias Kuhn <kuhntobias@gmail.com>
2020-07-09 15:40:45 +0200

    Improve nanopub extraction

--- fines/script.py
+++ fines/script.py
@@ -9,6 +9,11 @@ with open("data/GDPRfines.json") as json_file:
         f['@id'] = 'fine:'+str(f['id'])
         del f['id']
         del f['picture']
+        f['name'] = 'dbpedia:' + f['name'].strip().replace(" ", "_")
+        f['source'] = f['source'].strip()
+        if " " in f['source']:
+            print('Ignoring non-URL source: "' + f['source'] + '"')
+            del f['source']
         articles = re.finditer('Art. (.+?) GDPR', f['articleViolated'])
         if articles:
             f['articleViolated'] = []
@@ -23,15 +28,13 @@ with open("data/GDPRfines.json") as json_file:
 
     context = {}
     context['id'] = "http://example.com/id/"
-    context['fine'] = "http://example.com/fines"
+    context['fine'] = "http://example.com/fines/"
     context['authority'] = "http://example.com/authority"
     context['controller'] = "http://example.com/controller"
     context['date'] = "http://example.com/date"
-    context['name'] = "http://example.com/name"
-    context['picture'] = "http://example.com/picture"
     context['price'] = "http://example.com/price"
-    context['source'] = "dct:source"
     context['summary'] = "dct:description"
+    context['dbpedia'] = "http://dbpedia.org/resource/"
     context['gdpr'] = "http://purl.org/adaptcentre/resources/GDPRtEXT#article"
     context['dct'] = "http://purl.org/dc/terms/"
     fines = {}
@@ -42,6 +45,14 @@ with open("data/GDPRfines.json") as json_file:
     articlesViolated['@id'] = "http://example.com/articleViolated"
     articlesViolated['@type'] = "@id"
     context['articleViolated'] = articlesViolated
+    country = {}
+    country['@id'] = "http://example.com/country"
+    country['@type'] = "@id"
+    context['name'] = country
+    source = {}
+    source['@id'] = "dct:source"
+    source['@type'] = "@id"
+    context['source'] = source
     data['@context'] = context
 
     with open("data/GDPRfines_clean.jsonld", 'w') as outfile:
